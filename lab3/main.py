4e5d765fb614a0337296f00c77c1a7a831151be0
Vladislav Tarasov <v.tarasov@corp.mail.ru>
2019-10-27 19:06:01 +0300

    square

--- lab3/main.py
+++ lab3/main.py
@@ -1,8 +1,11 @@
 from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.square import Square
 
 def main():
     rectangle = Rectangle(3, 2, "black")
+    square = Square(5, "red")
     print(rectangle)
+    print(square)
 
 if __name__ == "__main__":
     main()
