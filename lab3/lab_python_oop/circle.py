6a43cfbed7f35be89268f7ba7c7caff2c0559bbf
Vladislav Tarasov <v.tarasov@corp.mail.ru>
2019-10-27 19:06:44 +0300

    Circle

---
+++ lab3/lab_python_oop/circle.py
@@ -0,0 +1,26 @@
+from lab_python_oop.figure import Figure
+from lab_python_oop.color import Color
+import math
+
+class Circle(Figure):
+    FIGURE_TYPE = "Circle"
+
+    @classmethod
+    def get_figure_type(cls):
+        return cls.FIGURE_TYPE
+
+    def __init__(self, radius, color):
+        self.radius = radius
+        self.color = Color()
+        self.color.color = color
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def __repr__(self):
+        return "{} color: {}, radius: {}, area: {}".format(
+            Circle.get_figure_type(),
+            self.color.color,
+            self.radius,
+            self.area()
+        )
